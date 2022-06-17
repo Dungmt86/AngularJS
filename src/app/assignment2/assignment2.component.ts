dae29583ef25fdf81965ce4d6c24cc20560d85c9
Dungmt86 <dungmt86@gmail.com>
2022-06-17 10:04:36 +0700

    assignment 2

---
+++ src/app/assignment2/assignment2.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "app-assignment2",
+  templateUrl:"./assignment2.component.html"
+})
+
+
+export class Assignment2Component {
+
+}
