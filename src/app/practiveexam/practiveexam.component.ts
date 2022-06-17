33c1eb15ccb03f882cbb9d41d8c06a20bab0d7d0
Dungmt86 <dungmt86@gmail.com>
2022-06-17 20:28:45 +0700

    exam

--- src/app/practiveexam/practiveexam.component.ts
+++ src/app/practiveexam/practiveexam.component.ts
@@ -1,15 +1,23 @@
 import {Component} from "@angular/core";
-import {IMail} from "../interfaces/mail.interface";
-import {ICategory} from "../interfaces/category.interface";
 import {WeatherService} from "../services/weather.service";
+import {user} from "../interfaces/user.interface";
+import {contact} from "../interfaces/contact.interface";
 
 @Component({
   selector: "app-practiveexam",
   templateUrl:"./practiveexam.component.html"
 })
 
-
 export class PractiveexamComponent {
-
+  user: user[] = [];
+  contact: contact[] = [];
+  constructor(private weatherService: WeatherService) {
+  }
+  ngOnInit(){
+    this.weatherService.contactList().subscribe(value => {
+      this.user = value.data.user;
+      this.contact = value.data.contact;
+    })
+  }
 
 }
