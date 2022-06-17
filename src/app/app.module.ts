9d04093f3e778294c22e807e674901e2d87a01be
Dungmt86 <dungmt86@gmail.com>
2022-06-17 19:59:36 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {StudentComponent} from "./student/student.component";
+import {ClassroomComponent} from "./classroom/classroom.component";
+import {ProductsComponent} from "./products/products.component";
+import {ProductComponent} from "./product/product.component";
+import {WeatherComponent} from "./weather/weather.component";
+
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import {Assigment1Component} from "./assigment1/assigment1.component";
+import {Assignment2Component} from "./assignment2/assignment2.component";
+import {ArrayComponent} from "./assignment2/array.component";
+import {PractiveexamComponent} from "./practiveexam/practiveexam.component";
+
+const appRoutes: Routes = [
+  {
+    path: 'lop-hoc', component: ClassroomComponent
+  },
+  {
+    path: 'san-pham', component: ProductsComponent
+  },
+  {
+    path: 'thoi-tiet', component: WeatherComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ClassroomComponent,
+    ClassroomComponent,
+    ProductsComponent,ProductComponent,
+    WeatherComponent,Assigment1Component,
+    Assignment2Component,ArrayComponent,
+    PractiveexamComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/practiveexam/practiveexam.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+import {IMail} from "../interfaces/mail.interface";
+import {ICategory} from "../interfaces/category.interface";
+import {WeatherService} from "../services/weather.service";
+
+@Component({
+  selector: "app-practiveexam",
+  templateUrl:"./practiveexam.component.html"
+})
+
+
+export class PractiveexamComponent {
+
+
+}
